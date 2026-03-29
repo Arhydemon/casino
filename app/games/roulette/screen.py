33e47f08a5676369528c02da06790d53915066ab
Arhydemon <1692328@gmail.com>
2026-03-29 14:21:16 +0300

    тех задание

--- app/games/roulette/screen.py
+++ app/games/roulette/screen.py
@@ -1,6 +1,17 @@
 import flet as ft
-from app.app_state import AppState
-from app.games.roulette.game import RouletteGame
+
+class Player:
+    def __init__(self):
+        self.balance = 1000
+
+class AppState:
+    def __init__(self):
+        self.player = Player()
+
+class RouletteGame:
+    def __init__(self, state):
+        self.state = state
+
 
 def show_roulette_screen(page: ft.Page, state: AppState, go_back) -> None:
     page.controls.clear()
@@ -9,13 +20,12 @@ def show_roulette_screen(page: ft.Page, state: AppState, go_back) -> None:
     title_text: ft.Text = ft.Text('Рулеточка', size=30)
     balance_text: ft.Text = ft.Text(f'Баланс: {state.player.balance}', size=30)
     info_text: ft.Text = ft.Text('Сделай ставку и жми СПИН', size=30)
-    
 
-    def back_to_menu(event: ft.ControlEvent) ->None:
+    def back_to_menu(event: ft.ControlEvent) -> None:
         go_back()
 
-    back_button: ft.ElevatedButton = ft.ElevatedButton('Домой', on_click=back_to_menu) 
-    
+    back_button: ft.ElevatedButton = ft.ElevatedButton('Домой', on_click=back_to_menu)
+
     roulette_column: ft.Column = ft.Column(
         controls=[title_text, balance_text, info_text, back_button],
         alignment=ft.MainAxisAlignment.CENTER,
@@ -26,13 +36,14 @@ def show_roulette_screen(page: ft.Page, state: AppState, go_back) -> None:
     page.add(roulette_column)
     page.update()
 
-def test_roulette_screen(page: ft.Page,) -> None:
+
+def test_roulette_screen(page: ft.Page) -> None:
     page.title = 'Тест рулетки'
     page.window.width = 1920
     page.window.height = 1080
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    state: AppState = AppState()  # создаём тестовое состояние приложения
+    state: AppState = AppState()
 
     def fake_back() -> None:
         page.controls.clear()
