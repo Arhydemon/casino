2806c8dc6f32f7bc2bc4138cef68aa7ff68634a8
Arhydemon <1692328@gmail.com>
2026-04-02 23:44:45 +0300

    сервачи

---
+++ app/core/services.py
@@ -0,0 +1,34 @@
+from app.app_state import AppState, Player
+from app.core.database import DatabaseManager
+from app.core.repositories import ProfileRepository, StatisticsRepository, SettingsRepository
+
+class AppStateService:
+    def __init__(self):
+        self.db = DatabaseManager() # создаётся объект для работы с БД
+        self.profile_repository = ProfileRepository(self.db) # cоздаётся репозиторий профиля, которому передаётся db
+        self.statistics_repository = StatisticsRepository(self.db) # такая же хуйня как выше
+        self.settings_repository = SettingsRepository(self.db)
+
+    def initialize_database(self) -> None:
+        self.db.initialize()
+
+    def load_state(self) -> AppState:
+        profile = self.profile_repository.get_profile()
+        if profile is None:
+            profile = self.profile_repository.create_default_profile()
+        statistics = self.statistics_repository.get_statistics(profile.id)
+        settings = self.settings_repository.get_settings(profile.id)
+        state = AppState() # создаём объект состояния приложения
+        state.player = Player(name=profile.name, balance=profile.balance)
+
+        if statistics is not None:
+            pass
+        if settings is not None:
+            pass
+        return state
+
+    def save_balance(self, state: AppState) -> None:
+        profile = self.profile_repository.get_profile()
+        if profile is None:
+            return
+        self.profile_repository.update_balance(profile.id, state.player.balance)
