9c13e2a2fc72d7b32207c5cea3449e98bb3cb42c
Arhydemon <1692328@gmail.com>
2026-04-21 19:22:33 +0300

    конфиг + readme нормальный + рофлик

---
+++ config.py
@@ -0,0 +1,26 @@
+# НАЗВАНИЕ
+APP_TITLE = "Дым Дым Казино и Бл*ди"
+
+# БД
+DB_PATH = "database/app.db"
+
+# ЛОГИ
+LOG_PATH = "log.txt"
+
+# БАЛИК ЕБАЛИК ХАХАХ
+DEFAULT_BALANCE = 5000
+
+# АССЕТЫ
+ASSETS_DIR = "assets"
+
+# ЗВУКИ
+SOUND_CLICK = "sounds/click.mp3"
+SOUND_ROULETTE_SPIN = "sounds/roulette_spin.mp3"
+SOUND_SLOTS_SPIN = "sounds/slots_spin.mp3"
+SOUND_WIN = "sounds/win.mp3"
+SOUND_LOSE = "sounds/lose.mp3"
+
+# ОКНО
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
+
