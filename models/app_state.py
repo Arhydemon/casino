5ce57761a85f0ddfa214917e060211a608ace9a0
Arhydemon <1692328@gmail.com>
2026-04-26 19:55:51 +0300

    feat(app_state): файл со всеми состояниями

---
+++ models/app_state.py
@@ -0,0 +1,17 @@
+from models.player import *
+from models.settings import *
+from models.statistics import *
+
+class AppState:
+    def __init__(self, player: Player, settings: Settings, statistics: Statistics) -> None:
+        self.player = player
+        self.statistics = statistics
+        self.settings = settings
+
+    def update_balance(self, amount: int) -> None:
+        self.player.balance = self.player.balance + amount
+
+    def record_game(self, win: bool, win_amount: int = 0) -> None:
+        self.statistics.add_game()
+        if win:
+            self.statistics.add_win(win_amount)
